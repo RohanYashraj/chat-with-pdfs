effb5a3e972dd9831086370327c0d06d952fc3d4
rohanyashraj <rohanyashraj@gmail.com>
2023-11-09 11:47:44 +0530

    Initial commit

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
+# import requests
+
+# url = "https://api-inference.huggingface.co/pipeline/text2text-generation/google/flan-t5-xxl"
+# response = requests.get(url, verify=False)
+
+
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -21,53 +27,62 @@ def get_pdf_text(pdf_docs):
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
+        separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
     )
     chunks = text_splitter.split_text(text)
     return chunks
 
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    # llm = ChatOpenAI()
+    llm = HuggingFaceHub(
+        repo_id="google/flan-t5-xxl",
+        model_kwargs={"temperature": 0.5, "max_length": 512},
+    )
 
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
+        llm=llm, retriever=vectorstore.as_retriever(), memory=memory
     )
     return conversation_chain
 
 
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
+    response = st.session_state.conversation({"question": user_question})
+    st.session_state.chat_history = response["chat_history"]
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(
+                user_template.replace("{{MSG}}", message.content),
+                unsafe_allow_html=True,
+            )
         else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(
+                bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True
+            )
 
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
+    st.set_page_config(
+        page_title="GI Accenture - ChatBot",
+        page_icon="./static/img/acc-logo.png",
+        layout="wide",
+        initial_sidebar_state="auto",
+        menu_items={
+            "Get Help": "https://www.extremelycoolapp.com/help",
+            "Report a bug": "https://www.extremelycoolapp.com/bug",
+            "About": "# This is a header. This is an *extremely* cool app!",
+        },
+    )
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -75,30 +90,66 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
+    # Add your logo using st.image
+    # st.image("./static/img/accenturelogo.png", width=300)
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+    # Add the header text using st.title
+    st.title("GI Accenture - Chat with PDFs")
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
+    # st.help("this is the help you asked for")
+    # st.header("GI Accenture - Chat with PDFs")
+    # st.write('### Component Example')
+    # st.write('<img src="./static/img/acc-logo.png"/>', unsafe_allow_html=True)
 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
+    # Create tabs
+    tab_titles = ["About", "Use Chatbot"]
+    tab1, tab2 = st.tabs(tab_titles)
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+    # Add content to each tab
+
+    with tab1:
+        with open("./static/about.md", "r") as file:
+            about_markdown = file.read()
+        st.markdown(about_markdown)
+
+        # st.write("Short Video")
+
+        # video_file = open("./static/vid/video.mp4", "rb")
+        # video_bytes = video_file.read()
+
+        # st.video(video_bytes)
+
+    with tab2:
+        # st.header("Topic A")
+        user_question = st.text_input("Ask a question about your documents:")
+        if user_question:
+            handle_userinput(user_question)
+
+        with st.sidebar:
+            # st.image("./static/img/accenturelogo.png", width=300)
+            st.image("./static/img/accenturelogo.png", use_column_width=True)
+            st.subheader("Your documents")
+            pdf_docs = st.file_uploader(
+                "Upload your PDFs here and click on 'Process'",
+                accept_multiple_files=True,
+            )
+            if st.button(label="Process"):
+                with st.spinner("Processing"):
+                    # get pdf text
+                    raw_text = get_pdf_text(pdf_docs)
+
+                    # get the text chunks
+                    text_chunks = get_text_chunks(raw_text)
+
+                    # create vector store
+                    vectorstore = get_vectorstore(text_chunks)
+
+                    # create conversation chain
+                    st.session_state.conversation = get_conversation_chain(vectorstore)
+                st.caption(
+                    "Click to initiate document loading. Loading documents without GPUs may result in longer processing times, especially when documents are being embedded concurrently."
+                )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ static/about.md
@@ -0,0 +1,37 @@
+## Transforming Actuarial Capabilities with Innovative Chatbot Technology
+
+### Introduction
+
+In the ever-evolving landscape of actuarial services, Accenture is on the brink of a significant transformation. The proposed case study introduces a groundbreaking application powered by state-of-the-art Generative AI large language models. This initiative aims to revolutionize actuarial processes by streamlining document handling and enabling effortless document-based queries. In this one-page document, we explore the details and implications of this transformative project.
+
+### The Need for Innovation
+
+Actuarial professionals at Accenture work with vast amounts of complex documents, making efficient data extraction and analysis a critical requirement. Conventional methods are time-consuming, resource-intensive, and often limited by human capacity. To address these challenges, the application leverages AI technology to simplify the actuarial workflow.
+
+### The Chatbot App (Proof of Concept)
+
+#### Core Features
+
+Our chatbot application is designed to handle numerous documents as inputs and provide a seamless interface for querying questions from these documents. Key features include:
+
+- **Document Ingestion:** Users can easily upload and manage multiple documents, reducing the time spent on manual data entry.
+- **Natural Language Querying:** The application allows for intuitive, conversational-style querying, making it accessible to all users, regardless of technical expertise.
+- **AI-Powered Insights:** Behind the scenes, powerful AI models generate responses, extract data, and provide relevant information in real-time.
+- **Scalability:** The application can scale to accommodate various document types and sizes, ensuring flexibility and adaptability.
+
+#### Benefits
+
+The advantages of the chatbot app for Accenture are multifaceted:
+
+- **Efficiency:** Streamlined document handling and real-time query responses significantly reduce the time and effort required for actuarial analysis.
+- **Consistency:** The application maintains a high level of accuracy and consistency in data extraction and analysis.
+- **Scalability:** As your document database grows, the application effortlessly scales to meet the demands of your actuarial team.
+- **Data-Driven Insights:** By harnessing AI capabilities, the app uncovers valuable insights from your documents, contributing to more informed decision-making.
+
+### The Vision
+
+This case study aims to demonstrate the potential impact of the chatbot application on Accenture's actuarial capabilities. By embracing advanced AI technology, we envision a future where actuarial professionals can focus on strategic decision-making rather than tedious data processing. The result: improved efficiency, reduced operational costs, and a competitive edge in the actuarial landscape.
+
+### Conclusion
+
+The proposed chatbot application serves as a testament to Accenture's commitment to innovation and excellence. By embracing cutting-edge AI technology, we can elevate actuarial capabilities, setting new standards in the industry. The case study offers a glimpse into the transformative power of technology and the promising future it holds for Accenture and its actuarial teams.
